e0abbd2116aa30e9f276efac40e0b0e7586cf8d5
juancruzlegarra <legarrajuan@gmail.com>
2023-04-01 02:34:03 -0300

    primeros cambios

---
+++ src/Login.jsx
@@ -0,0 +1,10 @@
+export const Login = () => {
+  let nombre = "Juan Cruz";
+
+  return (
+    <div>
+      <h1>Hola desde el componente Login</h1>
+      <h2>mi nombre es {nombre}</h2>
+    </div>
+  );
+};
